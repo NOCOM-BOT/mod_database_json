f6661c47821ad4195d3d2a47aad74d13299b2fb4
Lê Quang Lâm <badaimweeb@protonmail.com>
2022-01-09 13:39:46 +0700

    update

--- index.js
+++ index.js
@@ -17,17 +17,77 @@ if (worker.isMainThread) {
     process.exit(1);
 }
 
+/** @type {{[id: number]: JSONDatabase}} */
 let databaseList = {};
 class JSONDatabase {
     jsonLocation = "";
     dbName = "";
+    #data = {};
+    #lastRefresh = 0;
+    saveClock;
+    saving = false;
 
     constructor(jsonLocation, name) {
         this.jsonLocation = jsonLocation;
         this.dbName = name;
+
+        this.saveClock = setInterval(async () => {
+            if (!this.saving) {
+                this.saving = true;
+                await this.save();
+                this.saving = false;
+            }
+        })
     }
 
+    async refresh() {
+        if (this.#lastRefresh + 15000 <= Date.now()) {
+            let jsonData = JSON.parse(await fsPromises.readFile(this.jsonLocation));
+            this.#data = jsonData;
+        }
+    }
 
+    async save() {
+        await fsPromises.writeFile(JSON.stringify(this.#data, null, "\t"));
+    }
+
+    async getData(table, key) {
+        await this.refresh();
+        if (!Object.hasOwn(this.#data, table)) {
+            this.#data[table] = {};
+        }
+
+        return this.#data[table][key];
+    }
+
+    async setData(table, key, value) {
+        await this.refresh();
+        if (!Object.hasOwn(this.#data, table)) {
+            this.#data[table] = {};
+        }
+
+        this.#data[table][key] = value;
+    }
+
+    async deleteData(table, key) {
+        await this.refresh();
+
+        if (!Object.hasOwn(this.#data, table)) {
+            this.#data[table] = {};
+        }
+
+        delete this.#data[table][key];
+    }
+
+    async deleteTable(table) {
+        await this.refresh();
+
+        delete this.#data[table];
+    }
+
+    disconnect() {
+        clearInterval(this.saveClock);
+    }
 }
 
 parentPort.once("message", (data) => {
@@ -101,7 +161,77 @@ async function handleAPICall(cmd, data) {
             }
         case "connect_db":
             if (databaseList[data.databaseID]) throw "Database ID exists!";
-            throw "Not implemented";
+            databaseList[data.databaseID] = new JSONDatabase(data.params.file, data.databaseName);
+            return {
+                exist: true,
+                data: {
+                    success: true,
+                    databaseID: data.databaseID
+                }
+            }
+        case "get_data":
+            if (!(databaseList[data.databaseID] instanceof JSONDatabase)) throw "Database ID not found.";
+            try {
+                return {
+                    exist: true,
+                    data: {
+                        success: true,
+                        data: databaseList[data.databaseID].getData(data.table, data.key)
+                    }
+                }
+            } catch {
+                return {
+                    exist: true,
+                    data: {
+                        success: false,
+                        data: null
+                    }
+                }
+            }
+        case "set_data":
+            if (!(databaseList[data.databaseID] instanceof JSONDatabase)) throw "Database ID not found.";
+            try {
+                await databaseList[data.databaseID].setData(data.table, data.key, data.value);
+                return {
+                    exist: true,
+                    data: {
+                        success: true
+                    }
+                }
+            } catch {
+                return {
+                    exist: true,
+                    data: {
+                        success: false
+                    }
+                }
+            }
+        case "delete_data":
+            if (!(databaseList[data.databaseID] instanceof JSONDatabase)) throw "Database ID not found.";
+            await databaseList[data.databaseID].deleteData(data.table, data.key);
+            return {
+                exist: true,
+                data: {
+                    success: true
+                }
+            }
+        case "delete_table":
+            if (!(databaseList[data.databaseID] instanceof JSONDatabase)) throw "Database ID not found.";
+            await databaseList[data.databaseID].deleteTable(data.table);
+            return {
+                exist: true,
+                data: {
+                    success: true
+                }
+            }
+        case "disconnect":
+            if (!(databaseList[data.databaseID] instanceof JSONDatabase)) throw "Database ID not found.";
+            databaseList[data.databaseID].disconnect();
+            delete databaseList[data.databaseID];
+            return {
+                exist: true,
+                data: null
+            }
         default:
             return {
                 exist: false
